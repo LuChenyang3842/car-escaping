b9251200a21122753af0e34de4f16ceca351e817
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-16 15:41:29 +1100

    Add new file

---
+++ src/mycontroller/ Strategy.java
@@ -0,0 +1,23 @@
+package mycontroller;
+
+import java.util.ArrayList;
+import utilities.Coordinate;
+
+public abstract class Strategy {
+	public void getSuccessor(){
+		
+	}
+	
+	public void getGoal() {
+		
+	}
+	
+	public void findPath() {
+		
+	}
+	
+	public void heuristic() {
+		
+	}
+	
+}
