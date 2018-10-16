59d32e051bed2144450c5cb30906e84d363e3bc2
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-16 22:17:31 +1100

    Update HealStrategy.java

--- src/mycontroller/HealStrategy.java
+++ src/mycontroller/HealStrategy.java
@@ -1,9 +1,18 @@
 package mycontroller;
 
+import utilities.Coordinate;
+import tiles.HealthTrap;
+import tiles.MapTile;
+
 public class HealStrategy extends RoutingStrategy{
 	@Override
-	public void getGoal() {
-		// TODO Auto-generated method stub
-		
+	public boolean isGoal(Coordinate c) {
+		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
+		MapTile mapTile = recordTile.getMapTile();
+		if(mapTile instanceof HealthTrap) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
