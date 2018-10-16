415aaeea22d58b81a1eea825bd9d2e9f93a9c60b
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-16 22:15:28 +1100

    Update FindKeyStrategy.java

--- src/mycontroller/FindKeyStrategy.java
+++ src/mycontroller/FindKeyStrategy.java
@@ -1,11 +1,23 @@
 package mycontroller;
 
+import tiles.LavaTrap;
+import tiles.MapTile;
+import utilities.Coordinate;
+
 public class FindKeyStrategy extends RoutingStrategy {
 
 	@Override
-	public void getGoal() {
-		// TODO Auto-generated method stub
-		
+	public boolean isGoal(Coordinate c) {
+		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
+		MapTile mapTile = recordTile.getMapTile();
+		if (mapTile instanceof LavaTrap) {
+			LavaTrap lavaTile = (LavaTrap) mapTile;
+			
+			//what if two keys are beside each other?
+			if (lavaTile.getKey() != 0) {
+				return true;
+			}
+		} 
+		return false;
 	}
-
 }
