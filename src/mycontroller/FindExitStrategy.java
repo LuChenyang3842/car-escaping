4842761bc55fca021f606e5618c26b91ee92486e
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-16 22:14:36 +1100

    Update FindExitStrategy.java

--- src/mycontroller/FindExitStrategy.java
+++ src/mycontroller/FindExitStrategy.java
@@ -1,7 +1,17 @@
 package mycontroller;
 
+import tiles.MapTile;
+import utilities.Coordinate;
+
 public class FindExitStrategy extends RoutingStrategy {
-	public void getGoal() {
-		
+	public boolean isGoal(Coordinate c) {
+		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
+		MapTile mapTile = recordTile.getMapTile();
+		if (mapTile.isType(MapTile.Type.FINISH)) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 }
