c00a602884c1c80b1c975c8bd614743c06ee3f1c
LuChenyang3842 <LuChenyang3842@users.noreply.github.com>
2018-10-16 20:40:59 +1100

    midfied routing

--- src/mycontroller/RoutingStrategy.java
+++ src/mycontroller/RoutingStrategy.java
@@ -1,18 +1,32 @@
 package mycontroller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
 import utilities.Coordinate;
+import world.WorldSpatial.Direction;
 
 public abstract class RoutingStrategy {
-    private Coordinate currentLocation;
-	private HashMap<Coordinate, RecordTile> currentMap;
+	private Node currentNode;
 	
-	public void getSuccessor(){
+	public RoutingStrategy(Coordinate coordinate, Direction currentOrientation) {
+		currentNode = new Node(coordinate,currentOrientation);
+	}
+	
+	
+	public void getSuccessor(Node node){
+		Coordinate currentCoordinate = node.coordinate;
+		Direction currentOrientation = node.currentOrientation;
+		RecordTile currentTile = ExploreMap.getInstance().getExploredMap().get(currentCoordinate);
+		
+		
+		
 		
 	}
 	
 	
 	public void findPath() {
+		
 		
 	}
 	
@@ -20,6 +34,23 @@ public abstract class RoutingStrategy {
 		
 	}
 	public abstract boolean isGoal(Coordinate c);
+	
+	
+	
+	class Node {
+		Coordinate coordinate;
+		Direction currentOrientation;
+		
+		Node(Coordinate coordinate, Direction currentOrientation){
+			this.coordinate = coordinate;
+			this.currentOrientation =currentOrientation;
+		}
+		
+		
+		
+		
+		
+	}
 	
 	
 
