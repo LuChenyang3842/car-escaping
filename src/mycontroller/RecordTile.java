724252a5fca504df36caeec60f9663e690d0ae1e
Chenyang Lu <chenyangl5@student.unimelb.edu.au>
2018-10-16 16:43:15 +1100

    Upload RecordTile.java file

---
+++ src/mycontroller/RecordTile.java
@@ -0,0 +1,17 @@
+package mycontroller;
+
+import tiles.MapTile;
+
+public class RecordTile {
+	private tiles.MapTile.Type tileType;
+	private boolean explored;
+	public RecordTile(MapTile mapTile){
+		this.tileType = mapTile.getType();
+		this.explored = true;
+	}
+	
+	public tiles.MapTile.Type getType(){
+		return tileType;}
+	public boolean getExplored() {
+		return explored;}
+}
