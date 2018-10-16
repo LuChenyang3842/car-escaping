30448e76e41ef423cb606c9262483f1805718eb7
Chenyang Lu <chenyangl5@student.unimelb.edu.au>
2018-10-16 19:48:40 +1100

    Update RoutingStrategy.java

--- src/mycontroller/RoutingStrategy.java
+++ src/mycontroller/RoutingStrategy.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import utilities.Coordinate;
 
 public abstract class RoutingStrategy {
+    private Coordinate currentLocation;
+	private HashMap<Coordinate, RecordTile> currentMap;
+	
 	public void getSuccessor(){
 		
 	}
