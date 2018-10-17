1943a346db7d32471343c3ca2a26ba87b5772629
Chenyang Lu <chenyangl5@student.unimelb.edu.au>
2018-10-17 22:32:22 +1100

    Update FindKeyStrategy.java

--- src/mycontroller/FindKeyStrategy.java
+++ src/mycontroller/FindKeyStrategy.java
@@ -1,29 +1,25 @@
 package mycontroller;
 
-import tiles.LavaTrap;
 import tiles.MapTile;
 import utilities.Coordinate;
 import world.WorldSpatial.Direction;
 
-//Strategy to find the key, the goal is to get the key
-public class FindKeyStrategy extends RoutingStrategy {
-	int key;
-	public FindKeyStrategy(Coordinate coordinate, Direction currentOrientation, int targetKey) {
-		super(coordinate, currentOrientation);
-		key = targetKey;
+//Strategy to find the exit, the goal is to find the exit
+public class FindExitStrategy extends RoutingStrategy {
+	
+	public FindExitStrategy(Coordinate currentPosition, Direction orientation) {
+		super(currentPosition, orientation);
 	}
 
-	//check whether the coordinate has a key
+	//check whether the coordinate is an exit
 	@Override
 	public boolean isGoal(Coordinate c) {
 		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
 		MapTile mapTile = recordTile.getMapTile();
-		if (mapTile instanceof LavaTrap) {
-			LavaTrap lavaTile = (LavaTrap) mapTile;
-			if (lavaTile.getKey() == key) {
-				return true;
-			}
-		} 
-		return false;
+		if (mapTile.isType(MapTile.Type.FINISH)) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 }
