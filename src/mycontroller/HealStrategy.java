e56fbf4a6aa21910d8fc7fcca86631a8489caea6
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-17 19:24:07 +1100

    Update HealStrategy.java

--- src/mycontroller/HealStrategy.java
+++ src/mycontroller/HealStrategy.java
@@ -1,10 +1,18 @@
 package mycontroller;
 
 import utilities.Coordinate;
+import world.WorldSpatial.Direction;
 import tiles.HealthTrap;
 import tiles.MapTile;
 
+//Strategy to heal, the goal is to find the HealthTrap tile in ExploreMap
 public class HealStrategy extends RoutingStrategy{
+	
+	public HealStrategy(Coordinate currentPosition, Direction orientation) {
+		super(currentPosition, orientation);
+	}
+
+	//check whether the coordinate is a HealthTrap
 	@Override
 	public boolean isGoal(Coordinate c) {
 		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
@@ -14,5 +22,13 @@ public class HealStrategy extends RoutingStrategy{
 		} else {
 			return false;
 		}
+		/*if (mapTile.isType(MapTile.Type.TRAP)) {
+			TrapTile trapTile = (TrapTile)mapTile;
+			if(trapTile.getTrap().equals("health")) {
+				return true;
+			}
+		} 
+		return false;*/
+		
 	}
 }
