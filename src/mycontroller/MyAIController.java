aabbd94589c30383ef093095b370b846071b39df
LuChenyang3842 <LuChenyang3842@users.noreply.github.com>
2018-10-17 20:29:27 +1100

    update myAIcON

--- src/mycontroller/MyAIController.java
+++ src/mycontroller/MyAIController.java
@@ -12,10 +12,11 @@ import utilities.Coordinate;
 import world.Car;
 import world.WorldSpatial.Direction;
 
-public class MyAIController extends CarController{
+public class MyAIController extends CarController {
 	private HashMap<Coordinate, Integer> newKeyLocation;
 	private RoutingStrategy routingStrategy;
 	private int i = 0;
+
 	public MyAIController(Car car) {
 		super(car);
 		newKeyLocation = new HashMap<Coordinate, Integer>();
@@ -30,57 +31,46 @@ public class MyAIController extends CarController{
 		removeDuplicateKeys();
 		ExploreMap.getInstance().updateMap(currentView);
 		HashMap<Coordinate, RecordTile> myMap = ExploreMap.getInstance().getExploredMap();
-		
-		if(!newKeyLocation.isEmpty()) {
-			routingStrategy = new KeyMixStrategy(currentPosition, getOrientation(), getHealth(), newKeyLocation);
-		} else if (getKeys().size() == numKeys()) {
-			routingStrategy = new ExitMixStrategy(currentPosition, getOrientation(), getHealth());
-		} else {
-			routingStrategy = new ExploreStrategy(currentPosition, getOrientation());
-		}
-		
-		//routingStrategy = new ExploreStrategy(currentPosition, getOrientation()); //for testing
 		if (i == 0) {
 			applyForwardAcceleration();
 			i = i + 1;
-		}else {
-		
-		ArrayList<Coordinate> temp  = routingStrategy.AstarPathFinding(); //to be continued....
-		drive(currentPosition, temp.get(0));
-		//System.out.println (temp.get(0));
-		
+		} else {
+
+			if (!newKeyLocation.isEmpty()) {
+				routingStrategy = new KeyMixStrategy(currentPosition, getOrientation(), getHealth(), newKeyLocation);
+			} else if (getKeys().size() == numKeys()) {
+				routingStrategy = new ExitMixStrategy(currentPosition, getOrientation(), getHealth());
+			} else {
+				routingStrategy = new ExploreStrategy(currentPosition, getOrientation());
+			}
 		}
-		
-		
-		
+
 	}
-	
-	
+
+
 	private void drive(Coordinate start, Coordinate goal) {
-		int startX =  start.x;
+		int startX = start.x;
 		int startY = start.y;
 		int goalX = goal.x;
 		int goalY = goal.y;
 		if (goalX == startX + 1) {
-			System.out.println (1);
+			System.out.println(1);
 			goEast();
-		}
-		else if (goalX == startX - 1) {
+		} else if (goalX == startX - 1) {
 			goWest();
-		}
-		else if(goalY == startY+ 1) {
+		} else if (goalY == startY + 1) {
 			goNorth();
-		}else if (goalY == startY- 1) {
+		} else if (goalY == startY - 1) {
 			goSouth();
-		}else {
+		} else {
 			applyBrake();
 		}
-		
+
 	}
-	
+
 	private void goSouth() {
 		Direction currentOrientation = getOrientation();
-		switch(currentOrientation){
+		switch (currentOrientation) {
 		case EAST:
 			turnRight();
 			break;
@@ -97,14 +87,14 @@ public class MyAIController extends CarController{
 			applyBrake();
 			break;
 		}
-		
+
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void goNorth() {
 		Direction currentOrientation = getOrientation();
-		switch(currentOrientation){
+		switch (currentOrientation) {
 		case EAST:
 			turnLeft();
 			break;
@@ -121,12 +111,12 @@ public class MyAIController extends CarController{
 			applyBrake();
 			break;
 		}
-		
+
 	}
 
 	private void goEast() {
 		Direction currentOrientation = getOrientation();
-		switch(currentOrientation){
+		switch (currentOrientation) {
 		case EAST:
 			applyForwardAcceleration();
 			break;
@@ -143,12 +133,12 @@ public class MyAIController extends CarController{
 			applyBrake();
 			break;
 		}
-		
+
 	}
 
 	private void goWest() {
 		Direction currentOrientation = getOrientation();
-		switch(currentOrientation){
+		switch (currentOrientation) {
 		case EAST:
 			applyReverseAcceleration();
 			break;
@@ -165,13 +155,13 @@ public class MyAIController extends CarController{
 			applyBrake();
 			break;
 		}
-		
+
 	}
-	
-	//remove the key we already have
+
+	// remove the key we already have
 	private void removeDuplicateKeys() {
 		Iterator iter = newKeyLocation.entrySet().iterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			Map.Entry entry = (Map.Entry) iter.next();
 			Coordinate coordinate = (Coordinate) entry.getKey();
 			int key = (int) entry.getValue();
@@ -180,18 +170,18 @@ public class MyAIController extends CarController{
 			}
 		}
 	}
-	
-	//find new key in currentView(if any) and record its coordinate
+
+	// find new key in currentView(if any) and record its coordinate
 	private void findKeys(HashMap<Coordinate, MapTile> currentView) {
 		Iterator iter = currentView.entrySet().iterator();
-		while(iter.hasNext()) {
+		while (iter.hasNext()) {
 			Map.Entry entry = (Map.Entry) iter.next();
 			Coordinate coordinate = (Coordinate) entry.getKey();
 			MapTile mapTile = (MapTile) entry.getValue();
 			if (mapTile instanceof LavaTrap) {
 				LavaTrap lavaTrap = (LavaTrap) mapTile;
 				int key = lavaTrap.getKey();
-				if((key != 0) && (!getKeys().contains(key))) {
+				if ((key != 0) && (!getKeys().contains(key))) {
 					newKeyLocation.put(coordinate, key);
 				}
 			}
