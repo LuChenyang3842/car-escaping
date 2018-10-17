6d690c63d8ad7fd72b397ca63b9873161d082702
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-17 19:33:39 +1100

    Upload New File

---
+++ src/mycontroller/ExitMixStrategy.java
@@ -0,0 +1,38 @@
+package mycontroller;
+
+import tiles.HealthTrap;
+import tiles.MapTile;
+import utilities.Coordinate;
+import world.WorldSpatial.Direction;
+
+//a mix strategy to find the exit
+public class ExitMixStrategy extends RoutingStrategy{
+	HealStrategy heal;
+	FindExitStrategy findExit;
+	float health;
+	
+	public ExitMixStrategy(Coordinate coordinate, Direction currentOrientation, float health) {
+		super(coordinate, currentOrientation);
+		heal = new HealStrategy(coordinate, currentOrientation);
+		findExit = new FindExitStrategy(coordinate, currentOrientation);
+		this.health = health;
+	}
+
+	/*
+	 * if the health is low, find the health trap
+	 * otherwise find the exit
+	 */
+	@Override
+	public boolean isGoal(Coordinate c) {
+		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
+		MapTile mapTile = recordTile.getMapTile();
+		int healDistance = heal.AstarPathFinding().size();
+		//heal when standing on HealthTrap or close to HealthTrap(compare to exit)
+		if ((mapTile instanceof HealthTrap && (health < 100)) || 
+				((health < 50) && (healDistance != 0) && (healDistance < findExit.AstarPathFinding().size()))) {
+			return heal.isGoal(c);
+		} else {
+			return findExit.isGoal(c);
+		}
+	}
+}
