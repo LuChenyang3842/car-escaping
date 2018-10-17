373c07335c5d30e0bb0b23426db48cf48250bc66
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-17 12:21:49 +1100

    Update ExploreStrategy.java

--- src/mycontroller/ExploreStrategy.java
+++ src/mycontroller/ExploreStrategy.java
@@ -1,8 +1,16 @@
 package mycontroller;
 
 import utilities.Coordinate;
+import world.WorldSpatial.Direction;
 
+//Strategy to explore the map, the goal is to find a unexplored tile in ExploreMap
 public class ExploreStrategy extends RoutingStrategy {
+	
+	public ExploreStrategy(Coordinate currentPosition, Direction orientation) {
+		super(currentPosition, orientation);
+	}
+
+	//check whether the coordinate is unexplored
 	public boolean isGoal(Coordinate c) {
 		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
 		if (!recordTile.getExplored()) {
