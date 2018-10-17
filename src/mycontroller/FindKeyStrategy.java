b2419fada3b90ef94dbdb36abd3237c4cf110cdf
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-17 19:23:35 +1100

    Update FindKeyStrategy.java

--- src/mycontroller/FindKeyStrategy.java
+++ src/mycontroller/FindKeyStrategy.java
@@ -3,18 +3,24 @@ package mycontroller;
 import tiles.LavaTrap;
 import tiles.MapTile;
 import utilities.Coordinate;
+import world.WorldSpatial.Direction;
 
+//Strategy to find the key, the goal is to get the key
 public class FindKeyStrategy extends RoutingStrategy {
+	int key;
+	public FindKeyStrategy(Coordinate coordinate, Direction currentOrientation, int targetKey) {
+		super(coordinate, currentOrientation);
+		key = targetKey;
+	}
 
+	//check whether the coordinate has a key
 	@Override
 	public boolean isGoal(Coordinate c) {
 		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
 		MapTile mapTile = recordTile.getMapTile();
 		if (mapTile instanceof LavaTrap) {
 			LavaTrap lavaTile = (LavaTrap) mapTile;
-			
-			//what if two keys are beside each other?
-			if (lavaTile.getKey() != 0) {
+			if (lavaTile.getKey() == key) {
 				return true;
 			}
 		} 
