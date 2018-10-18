a0aa3a90baa792386c2729295123e84be566bc4a
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-18 18:17:11 +1100

    Update HealStrategy.java

--- src/mycontroller/HealStrategy.java
+++ src/mycontroller/HealStrategy.java
@@ -4,8 +4,21 @@ import utilities.Coordinate;
 import world.WorldSpatial.Direction;
 import tiles.HealthTrap;
 import tiles.MapTile;
-
-//Strategy to heal, the goal is to find the HealthTrap tile in ExploreMap
+/**
+ * The University of Melbourne
+ * SWEN30006 Software Modelling and Design
+ * FileName: HealStrategy.java
+ *
+ *
+ * This class contains the Strategy to find health trap, 
+ * the goal is to find the HealthTrap tile in ExploreMap
+ *
+ * @author  Chenyang Lu, Leewei Kuo, Xueting Tan
+ * @StudentID 951933, 932975, 948775
+ * @Username  chenyangl5, leeweik1, xuetingt
+ * 
+ * @Date  18/10/2018 
+ */
 public class HealStrategy extends RoutingStrategy{
 	
 	public HealStrategy(Coordinate currentPosition, Direction orientation) {
@@ -21,7 +34,6 @@ public class HealStrategy extends RoutingStrategy{
 			return true;
 		} else {
 			return false;
-		}
-		
+		}	
 	}
 }
