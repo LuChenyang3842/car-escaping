1b4b898591ba784a3ead84a1683e468f197920d7
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-18 17:45:20 +1100

    Update FindKeyStrategy.java

--- src/mycontroller/FindKeyStrategy.java
+++ src/mycontroller/FindKeyStrategy.java
@@ -6,26 +6,42 @@ import tiles.LavaTrap;
 import tiles.MapTile;
 import utilities.Coordinate;
 import world.WorldSpatial.Direction;
-
-//Strategy to find the key, the goal is to get the key
+/**
+ * The University of Melbourne
+ * SWEN30006 Software Modelling and Design
+ * FileName: ExploreStrategy.java
+ *
+ *
+ * This class contains Strategy to find the key, 
+ * the goal is to find a key
+ *
+ * @author  Chenyang Lu, Leewei Kuo, Xueting Tan
+ * @StudentID 951933, 932975, 948775
+ * @Username  chenyangl5, leeweik1, xuetingt
+ * 
+ * @Date  18/10/2018 
+ */
 public class FindKeyStrategy extends RoutingStrategy {
-	Set<Integer> key;
+	
+	//the key we already have
+	private Set<Integer> holdKey;
+	
 	public FindKeyStrategy(Coordinate coordinate, Direction currentOrientation, Set<Integer> key) {
 		super(coordinate, currentOrientation);
-		this.key = key;
+		this.holdKey = key;
 	}
-	
-	//check whether the coordinate has a key
-		@Override
-		public boolean isGoal(Coordinate c) {
-			RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
-			MapTile mapTile = recordTile.getMapTile();
-			if (mapTile instanceof LavaTrap) {
-				LavaTrap lavaTile = (LavaTrap) mapTile;
-				if (lavaTile.getKey() != 0 && !key.contains(lavaTile.getKey())) {
-					return true;
-				}
-			} 
-			return false;
-		}
+		
+	//check whether the coordinate has an unseen key
+	@Override
+	public boolean isGoal(Coordinate c) {
+		RecordTile recordTile = ExploreMap.getInstance().getExploredMap().get(c);
+		MapTile mapTile = recordTile.getMapTile();
+		if (mapTile instanceof LavaTrap) {
+			LavaTrap lavaTile = (LavaTrap) mapTile;
+			if (lavaTile.getKey() != 0 && !holdKey.contains(lavaTile.getKey())) {
+				return true;
+			}
+		} 
+		return false;
 	}
+}
