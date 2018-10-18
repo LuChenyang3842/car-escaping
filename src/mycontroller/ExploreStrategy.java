843f4f20cce4c61da465b94d2440da7ac6b266a4
Leewei Kuo <leeweik1@student.unimelb.edu.au>
2018-10-18 17:37:35 +1100

    Update ExploreStrategy.java

--- src/mycontroller/ExploreStrategy.java
+++ src/mycontroller/ExploreStrategy.java
@@ -2,8 +2,21 @@ package mycontroller;
 
 import utilities.Coordinate;
 import world.WorldSpatial.Direction;
-
-//Strategy to explore the map, the goal is to find a unexplored tile in ExploreMap
+/**
+ * The University of Melbourne
+ * SWEN30006 Software Modelling and Design
+ * FileName: ExploreStrategy.java
+ *
+ *
+ * This class contains strategy to explore the map, 
+ * the goal is to find a unexplored tile in ExploreMap
+ *
+ * @author  Chenyang Lu, Leewei Kuo, Xueting Tan
+ * @StudentID 951933, 932975, 948775
+ * @Username  chenyangl5, leeweik1, xuetingt
+ * 
+ * @Date  18/10/2018 
+ */
 public class ExploreStrategy extends RoutingStrategy {
 	
 	public ExploreStrategy(Coordinate currentPosition, Direction orientation) {
