627f68fc3e795a941dac3e231374095fd6c03019
Chenyang Lu <chenyangl5@student.unimelb.edu.au>
2018-10-18 14:26:01 +1100

    Update RoutingStrategy.java

--- src/mycontroller/RoutingStrategy.java
+++ src/mycontroller/RoutingStrategy.java
@@ -11,34 +11,35 @@ import world.WorldSpatial;
 import world.WorldSpatial.Direction;
 
 public abstract class RoutingStrategy {
-	private Node startNode;
-	private final static int ACTION_COST = 1;
+	private Node startNode;  // Node is the inner class, the code is located at the end
+	private final static int ACTION_COST = 1; // make all the cost of each action equal to 1
 
 	public RoutingStrategy(Coordinate coordinate, Direction currentOrientation) {
 		startNode = new Node(coordinate, currentOrientation);
 	}
-
+	
 	/*
-	 * use A star algorithm to find path would return an arrayList of coordinate to
-	 * get to goal state if goal state is not reachable, return an empty
-	 * arrayList<Coordinate> if startNode is goal, return arrayList that only
-	 * contain start coordinate
+	 * This is the A* algorithm which used to find path
+	 * 
+	 * The algorithm finds a list of coordinate from start state to goal state
+	 * 
+	 * @return an arrayList<Coordinate> which contains a list of Coordinate to goal state
+	 * 
 	 */
 	public ArrayList<Coordinate> AstarPathFinding() {
-		PriorityQueue<Node> openList = new PriorityQueue<>();
-		ArrayList<Coordinate> closedList = new ArrayList<Coordinate>();
+		PriorityQueue<Node> openList = new PriorityQueue<>(); // The data structure for openlist is priority queue
+		ArrayList<Coordinate> closedList = new ArrayList<Coordinate>(); // data structure for open-list
 		openList.add(startNode);
 		if (isGoal(startNode.getCoordinate())) {
 			ArrayList<Coordinate> stop = new ArrayList<Coordinate>();
 			stop.add(startNode.getCoordinate());
 			return stop;
 		}
-
 		while (!openList.isEmpty()) {
 			Node currentNode = openList.remove();
 			int currentCost = currentNode.getG();
 			ArrayList<Coordinate> currentPathToTheNode = currentNode.getpathTotheNode();
-			if (!containsCoordinate(currentNode.getCoordinate(),closedList)) {
+			if (!closedList.contains(currentNode.getCoordinate())) {
 				closedList.add(currentNode.getCoordinate());
 				if (isGoal(currentNode.getCoordinate())) {
 					return currentPathToTheNode;
@@ -48,7 +49,7 @@ public abstract class RoutingStrategy {
 					for (Node successorNode : successors) {
 						int g = currentNode.getG() + ACTION_COST;
 						int h = heuristic(successorNode.getCoordinate());
-						if (!containsCoordinate(successorNode.getCoordinate(),closedList)) {
+						if (!closedList.contains(successorNode.getCoordinate())) {
 							currentPathToTheNode = currentNode.getpathTotheNode(); // get an new deep copy of array
 							currentPathToTheNode.add(successorNode.getCoordinate());
 							int f = g + h;
@@ -64,17 +65,18 @@ public abstract class RoutingStrategy {
 		return new ArrayList<Coordinate>();
 	}
 	
-	private boolean containsCoordinate(Coordinate coordinate,ArrayList<Coordinate> arrayOfCoordinate){
-		if(arrayOfCoordinate.size() == 0)
-			return false;
-		for (Coordinate temp: arrayOfCoordinate) {
-			if (temp.equals(coordinate)) 
-				return true;
-		}
-		return false;
-	}
-
-	public ArrayList<Node> getSuccessor(Node node) {
+	
+	/*
+	 * This is helper method for A* algorithm to find successor of certain node
+	 * 
+	 * For each Legal actions, only if next coordinate is not wall or mud, it would
+	 * be defined as successor
+	 * 
+	 * @param node: an object of inner class Node which contains current Direction and coordinate
+	 * @return arralist<Node> that contains all the successor of the node
+	 */
+	
+	private ArrayList<Node> getSuccessor(Node node) {
 		Coordinate currentCoordinate = node.coordinate;
 		int x = currentCoordinate.x;
 		int y = currentCoordinate.y;
@@ -101,8 +103,6 @@ public abstract class RoutingStrategy {
 			default:
 				successorCoordinate = new Coordinate(x, y); // should never happen
 			}
-//			System.out.println(direction);
-//			System.out.println(successorCoordinate);
 			if (successorCoordinate.x < 0 || successorCoordinate.y < 0) {
 				continue;
 			}
@@ -120,14 +120,27 @@ public abstract class RoutingStrategy {
 
 	}
 
-	public ArrayList<Direction> getLegalDirections(Node node) {
+	/*
+	 * This is helper method for get successor method
+	 * 
+	 * If the current maptile is road, the arraylist contains four direction -w,n,s,e
+	 * If the current maptile is grass/start/healthTrap, the legal actions are 
+	 * current-direction & reverse-direction
+	 * 
+	 * @param  node  an object of inner class Node which contains current Direction and coordinate
+	 * @return arrayList<Directiom> that contains for legal actions
+	 */
+	private ArrayList<Direction> getLegalDirections(Node node) {
 		ArrayList<Direction> legalActions = new ArrayList<Direction>();
 		Coordinate currentCoordinate = node.coordinate;
 		Direction currentOrientation = node.currentOrientation;
 		MapTile currentTile = ExploreMap.getInstance().getExploredMap().get(currentCoordinate).getMapTile();
 		// if the MapTile type is grass, then the legal action would only be current
 		// action/ reverse action
-		if (currentTile.isType(MapTile.Type.TRAP) && ((TrapTile) currentTile).getTrap().equals("grass")) {
+		if (currentTile.isType(MapTile.Type.TRAP) && ((TrapTile) currentTile).getTrap().equals("grass") ||
+				currentTile.isType(MapTile.Type.START) ||
+				currentTile.isType(MapTile.Type.TRAP) && ((TrapTile) currentTile).getTrap().equals("health")
+				) {
 			legalActions.add(currentOrientation);
 			legalActions.add(WorldSpatial.reverseDirection(currentOrientation));
 			return legalActions;
@@ -140,6 +153,15 @@ public abstract class RoutingStrategy {
 		return legalActions;
 	}
 
+	/*
+	 * This is the heuristic function
+	 * 
+	 * to set the heuristic, the a* algorithm would try to avoid dangerous trap 
+	 * as much as possible
+	 * 
+	 * @param  coordinate
+	 * @return if the map tile of current coordinate is 100, then 
+	 */
 	public int heuristic(Coordinate coordinate) {
 		MapTile currentTile = ExploreMap.getInstance().getExploredMap().get(coordinate).getMapTile();
 		if (currentTile.isType(MapTile.Type.TRAP) && ((TrapTile) currentTile).getTrap().equals("lava"))
@@ -147,28 +169,42 @@ public abstract class RoutingStrategy {
 		return 0;
 	}
 
-	public abstract boolean isGoal(Coordinate c);
+	/*
+	 * This the abstract method that is supposed to be override by descender class
+	 * 
+	 * @param  coordinate of a node
+	 * 
+	 * @return true if the coordinate is the goal
+	 */
+	public abstract boolean isGoal(Coordinate coordinate);
 
+	
+
+	
+	
+	/*
+	 * This the inner class which defines the node, which mainly used to ease 
+	 * the implementation of A* algorithm
+	 * 
+	 */
 	class Node implements Comparable<Node> {
-		Coordinate coordinate;
-		Direction currentOrientation;
+		Coordinate coordinate; // the coordinate of the node
+		Direction currentOrientation; // the direction that reaches the node
 		ArrayList<Coordinate> pathTotheNode = new ArrayList<Coordinate>();
-		int g = 0;
-		int f = 0;
+		int g = 0;// the total action cost
+		int f = 0;// the total f value where f= g + h
 
 		Node(Coordinate coordinate, Direction currentOrientation) {
 			this.coordinate = coordinate;
 			this.currentOrientation = currentOrientation;
 		}
-
 		Node(Coordinate coordinate, Direction currentOrientation, int g, int f) {
 			this.coordinate = coordinate;
 			this.currentOrientation = currentOrientation;
 			this.g = g;
 			this.f = f;
 		}
-
-		// return a deep copy of arrayList
+		// return a deep copy of arrayList of coordinate
 		ArrayList<Coordinate> getpathTotheNode() {
 			ArrayList<Coordinate> temp = new ArrayList<Coordinate>();
 			if (pathTotheNode.size() > 0) {
@@ -202,6 +238,7 @@ public abstract class RoutingStrategy {
 			return currentOrientation;
 		}
 
+		// The compareTo method is used to help the implement of PriorityQueue<Node>
 		@Override
 		public int compareTo(Node node) {
 			if (this.getF() > node.getF()) {
